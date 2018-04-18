3045d40487767d2518dcb7fb1a6e61b84f209503
hatelove <jay.hatelove@gmail.com>
2018-04-18 17:28:57 +0800

    Add project files.

---
+++ TennisScore/TennisGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisScore
+{
+    public class TennisGame
+    {
+        private readonly IRepository<Game> _repo;
+
+        public TennisGame(IRepository<Game> repo)
+        {
+            _repo = repo;
+        }
+
+        public string ScoreResult(int gameId)
+        {
+            return this._repo.GetGame(gameId).ScoreResult();
+        }
+    }
+}
